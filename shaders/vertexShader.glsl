0c9b829ea460b77d43f1c578f5193faece64bbeb
monk <nathan.tihon@student.uclouvain.be>
2023-10-12 01:36:54 +0200

    Learning color attributes

--- shaders/vertexShader.glsl
+++ shaders/vertexShader.glsl
@@ -1,5 +1,9 @@
 #version 460 core
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec3 color;
+out vec4 vertexColor;
+
 void main(){
-   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+   gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
+   vertexColor = vec4(color, 1.0);
 }
--- src/graph.cpp
+++ src/graph.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv){
         printf("Failed to initialize glfw\n");
         return EXIT_FAILURE;
     }
-    glfwWindowHint(GLFW_SAMPLES, 2); // 4x antialiasing
+    glfwWindowHint(GLFW_SAMPLES, 2); // 2x antialiasing
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -30,13 +30,14 @@ int main(int argc, char** argv){
         return EXIT_FAILURE;
     }
     glfwMakeContextCurrent(window);
+
+    // Callback for the sizing of the window (allow resizing)
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
     if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
         printf("Failed to initialize GLAD");
         return -1;
     }
-
-    // Callback for the sizing of the window (allow resizing)
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // Compiling shader for vertices
     GLuint vertexShader = loadShaders("./shaders/vertexShader.glsl", GL_VERTEX_SHADER);
@@ -54,37 +55,46 @@ int main(int argc, char** argv){
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
-    };
-
     // Create the vertex array object
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-    // Create the verted buffer object
+    float vertices[] = {
+    //    x      y     R     G     B
+        -0.5f, -0.5f, 1.0f, 0.5f, 0.2f,
+         0.5f, -0.5f, 0.0f, 0.5f, 0.2f,
+        -0.5f,  0.5f, 1.0f, 0.5f, 0.2f,
+         0.6f, -0.5f, 1.0f, 0.5f, 0.4f,
+        -0.4f,  0.5f, 0.0f, 0.5f, 0.4f,
+         0.6f,  0.5f, 1.0f, 0.5f, 0.4f
+    };
+
+    // Create the vertex buffer object
     GLuint VBO;
     glGenBuffers(1, &VBO);   // Generate the buffer
     glBindBuffer(GL_ARRAY_BUFFER, VBO);  // Make it the current openGL object
     // Add data to the current buffer 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
     // 3. then set our vertex attributes pointers
     glVertexAttribPointer(
             0,                  // must math the layout in the shader
-            3,                  // size
+            2,                  // size
             GL_FLOAT,           // Type
-            GL_TRUE,            // Are the coords normalized ?
-            0,                  // Stride
+            GL_FALSE,           // Are the coords normalized ?
+            5*sizeof(float),    // Stride
             (void*)0            // offset
     );
     glEnableVertexAttribArray(0);
+    glVertexAttribPointer(
+            1,                       // must math the layout in the shader
+            3,                       // size
+            GL_FLOAT,                // Type
+            GL_TRUE,                 // Are the coords normalized ?
+            5*sizeof(float),         // Stride
+            (void*)(2*sizeof(float)) // offset
+    );
+    glEnableVertexAttribArray(1);
 
     // FPS seems to be set at 60 for my laptop
     while(!glfwWindowShouldClose(window)) {
@@ -92,6 +102,7 @@ int main(int argc, char** argv){
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         glDrawArrays(GL_TRIANGLES, 0, 6);
+
         glfwSwapBuffers(window);
     }
 
